779d017994c04b3814485d06af9a1bcbd2644a99
Ermesson Lima <ermessonlimadossantos@hotmail.com>
2022-03-06 10:26:58 -0300

    feat: photo associate

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,11 @@ import Sequilize from 'sequelize';
 import databaseConfig from '../config/database';
 import Aluno from '../models/Aluno';
 import User from '../models/User';
+import Photo from '../models/Photo';
 
-const models = [Aluno, User];
+const models = [Aluno, User, Photo];
 
 const connection = new Sequilize(databaseConfig);
 
 models.forEach((model) => model.init(connection));
+models.forEach((model) => model.associate && model.associate(connection.models));
---
+++ src/database/migrations/20220306131000-criar-tabela-de-foto-do-aluno.js
@@ -0,0 +1,39 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('photos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      originalname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      alunos_ide: {
+        type: Sequelize.INTEGER,
+        references: { model: 'alunos', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('photos');
+  },
+};
